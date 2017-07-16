210f3f712736b351cd54a3cba7a3b2f2cdb23b45
Jenn <jhu890@gmail.com>
2017-07-16 10:57:52 -0700

    added user input

--- tests.py
+++ tests.py
@@ -10,22 +10,50 @@ def updateWeights(w, H, goals, alpha_0, alpha_1):
         w[i] = alpha_0 * g.overlap(C) + alpha_1 * g.likelihood(H)
     return w
 
-def test():
+def optimalGoals(w, goals):
+    return [goals[i] for i, x in enumerate(w) if x == max(w)]
+
+def initialize():
     goals = generateGoals()
     deck = Deck(0.5)
     w = np.zeros(len(goals))
     H = History()
+    return goals, deck, w, H
 
+def play():
+    goals, deck, w, H = initialize()
     H.hands = deck.draw(6)
 
-    for i in xrange(5):
+    while not any(g.overlap(H.hands) == 6 for g in goals):
         H.table = deck.draw(4)
         r = deck.reshuffle(H.table)
-        H.update(H.hands + H.table, r)
-        w = updateWeights(w, H, goals, 1, 1)
-        optimalGoals = [goals[i] for i, x in enumerate(w) if x == max(w)]
-        print H
-        print "Optimal goals: ", optimalGoals
+
+        print "----> HANDS: ", H.hands
+        print "----> TABLE: ", H.table
+
+        optimalBefore = optimalGoals(w, goals)
+        print "Optimal goals before swap: ", optimalBefore
+
+        # prompt user to enter which cards to swap
+        i_list = input('Enter list of indices to swap from hands: ')
+        j_list = input('Enter list of indices to swap from table: ')
         print
 
-test()
+        if len(i_list) != len(j_list):
+            print 'Must swap same number from hands and table.'
+        else:
+            for i in i_list:
+                for j in j_list:
+                    H.hands[i], H.table[j] = H.table[j], H.hands[i]
+
+        H.update(H.hands + H.table, r)
+        w = updateWeights(w, H, goals, 1, 1)
+        optimalAfter = optimalGoals(w, goals)
+        print H
+        print "Optimal goals after swap: ", optimalAfter
+        print
+
+    print 'CONGRATULATIONS! You\'ve found a straight flush!'
+    print optimalGoals(w, goals)[0].cards
+    
+play()
